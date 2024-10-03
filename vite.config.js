37945658c2a8d8ebce2919baeafaa907743c428e
Sudipoffice <mandalsudipoffice@gmail.com>
2024-10-03 12:00:19 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Taskify/',
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['react-icons/fa'], // Add any external modules if needed
-    },
+      external: ["react-icons/bi"]
+    }
   },
-});
+})
